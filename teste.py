65b946632f91acb2051aa20c99194c9b8b825bc1
Borges <borgespaulo72@yahoo.com.br>
2026-09-12 19:28:41 -0300

    feat: Aulas Faculdade python

---
+++ teste.py
@@ -0,0 +1,25 @@
+idade = 54
+nome = "Borges"
+altura = 1.75
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+idade = 53
+print("Nova idade:", idade)
+if idade >= 18:
+    print("Voce e maior de idade.")
+else:
+    print("Voce e menor de idade.")
+
+contador = 0
+while contador < 5:
+    print("Contador e:", contador)
+    contador += 1    
+
+def func():
+   x = 1
+   print(x)
+x = 10
+func()
+print(x)
+print('Ola, Mundo!')
