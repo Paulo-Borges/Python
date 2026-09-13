ec592e9201e17e53188059361d1f974b6ca97aaf
Borges <borgespaulo72@yahoo.com.br>
2026-09-13 13:28:59 -0300

    feat: desafio 8 a 10

---
+++ guanabara/desafio8.py
@@ -0,0 +1,4 @@
+medida = float(input('Uma distacia em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A medida de {}m metros corresponde a {}cm centímetros e {}mm milímetros.'.format(medida, cm, mm))
