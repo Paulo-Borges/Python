26abb112c295724c8196e81de11c6976a7409182
Borges <borgespaulo72@yahoo.com.br>
2026-09-13 15:30:02 -0300

    fix: Incluindo os exercicios

--- guanabara/ManipulandoString.py
+++ guanabara/ManipulandoString.py
@@ -8,3 +8,16 @@ print('Curso' in frase)
 print(frase.find('Video'))
 print(frase.split())
 print(frase.strip())
+
+preço = float(input('Digite o preço: R$'))
+novo = preço - (preço * 5 / 100)
+print('O preço com 5% de desconto é R${:.2f}'.format(novo))
+
+salario = float(input('Digite o salário: R$'))
+novo_salario = salario + (salario * 15 / 100)
+print('O salário com 15% de aumento é R${:.2f}'.format(novo_salario))
+
+dias = int(input('Quantos dias de aluguel? '))
+km = float(input('Quantos km rodados? '))
+preço_total = (dias * 60) + (km * 0.15)
+print('O total a pagar é R${:.2f}'.format(preço_total))
